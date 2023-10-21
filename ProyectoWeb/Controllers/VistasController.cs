c08a1dfeaf8f6f04509237a52559eb4fb6517743
Gustavovmdx <Gustavovmdx@gmail.com>
2023-10-20 22:07:53 -0500

    Primer Commit

---
+++ ProyectoWeb/Controllers/VistasController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoWeb.Controllers
+{
+    public class VistasController : Controller
+    {
+        public IActionResult Main()
+        {
+            return View();
+        }
+
+        public IActionResult Preguntas()
+        {
+            return View();
+        }
+
+        public IActionResult Nosotros()
+        {
+            return View();
+        }
+
+
+    }
+}
